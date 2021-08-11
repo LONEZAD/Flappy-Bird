17ea66cd8be7c48a8ec925d0a70e20c54fb5e96c
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-08-11 17:48:34 +0530

    Menue handler was implemented

--- src/application_handler.py
+++ src/application_handler.py
@@ -1,14 +1,35 @@
 from game.game_handler import GameHandler
+from src.menu.menu_handler import MenuHandler
+from typing import Union
 
 
 class ApplicationHandler:
-    __game_handler: GameHandler
+    __game_handler: Union[GameHandler, None]
+    __menu_handler: Union[MenuHandler, None]
 
-    def __init__(self):
+    def __init__(self) -> None:
+        self.__end_game()
+
+    def __start_game(self) -> None:
         self.__game_handler = GameHandler()
+        self.__menu_handler = None
 
-    def update(self):
-        self.__game_handler.update()
+    def __end_game(self) -> None:
+        self.__game_handler = None
+        self.__menu_handler = MenuHandler()
 
-    def render(self):
-        self.__game_handler.render()
+    def update(self) -> None:
+        if self.__game_handler is not None:
+            self.__game_handler.update()
+            if self.__game_handler.shold_game_start:
+                self.__end_game()
+        if self.__menu_handler is not None:
+            self.__menu_handler.update()
+            if self.__menu_handler.should_game_start:
+                self.__start_game()
+
+    def render(self) -> None:
+        if self.__game_handler is not None:
+            self.__game_handler.render()
+        if self.__menu_handler is not None:
+            self.__menu_handler.render()
---
+++ src/menu/menu_handler.py
@@ -0,0 +1,13 @@
+class MenuHandler:
+    def __init__(self) -> None:
+        pass
+
+    def update(self) -> None:
+        pass
+
+    def render(self) -> None:
+        pass
+
+    @property
+    def should_game_start(self) -> bool:
+        return True  # A dummy return value till the nenu is implemented
