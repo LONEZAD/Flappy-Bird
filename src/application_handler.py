9b3e05e4651899792875139e0cd68ed77a260da4
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-08-11 16:53:08 +0530

    Added the application handler

---
+++ src/application_handler.py
@@ -0,0 +1,9 @@
+class ApplicationHandler:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
--- src/main.py
+++ src/main.py
@@ -3,16 +3,19 @@ import pygame
 
 from window_handler import WindowHandler
 from event_handler import EventHandler
+from application_handler import ApplicationHandler
 
 
 class Main:
     __window_handler_instance: WindowHandler
     __event_handler_instance: EventHandler
+    __application_handler: ApplicationHandler
 
     def __init__(self) -> None:
         self.__window_handler_instance = WindowHandler()
         self.__event_handler_instance = EventHandler()
         self.__event_handler_instance.add_event_callback(pygame.QUIT, self.__window_exit_callback)
+        self.__application_handler = ApplicationHandler()
         self.__run()
 
     def __run(self) -> None:
@@ -22,10 +25,12 @@ class Main:
 
     def __update(self):
         self.__event_handler_instance.update()
+        self.__application_handler.update()
 
     def __render(self):
         self.__window_handler_instance.pygame_window_surface.fill((0, 0, 0))
         pygame.display.flip()
+        self.__application_handler.render()
 
     @staticmethod
     def __window_exit_callback() -> None:
