c72c15bfb7301b7b9cdbbf99f3ce6840825eb184
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-08-19 10:05:42 +0530

    Added circular rigid bodies

---
+++ src/game/game_objects/rigid_body.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from src.game.game_objects.move_able_game_object import MoveAbleGameObject
+
+
+class RectangleRigidBody(MoveAbleGameObject, ABC):
+    @property
+    @abstractmethod
+    def size(self) -> [float, float]:
+        """Should return the width, height"""
+
+
+class CircularRigidBody(MoveAbleGameObject, ABC):
+    def __init__(self):
+        super().__init__()
+        self._set_image("circle_default.png")
+
+    @property
+    @abstractmethod
+    def radius(self) -> float:
+        """Should return the """
